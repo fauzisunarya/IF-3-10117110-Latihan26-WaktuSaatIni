6c9b26bd94d01f4b885c3eccf4394aed99a8e362
fauzisunarya <fauzialdzikirullah@gmail.com>
2018-10-01 18:40:30 +0700

    first commit

---
+++ src/pkgif/pkg3/pkg10117110/latihan26/waktusaatini/IF310117110Latihan26WaktuSaatIni.java
@@ -0,0 +1,30 @@
+
+
+
+/**
+ *
+ * @author
+ * Nama : Fauzi Sunarya
+ * Kelas : IF-3
+ * NIM : 10117110
+ * Deskripsi Program : Program Jenis ini Berisi Program Untuk Menampilkan
+ * Hari Tanggal dan Waktu Saat ini
+ */
+package pkgif.pkg3.pkg10117110.latihan26.waktusaatini;
+
+
+import java.util.Date;  
+   
+public class IF310117110Latihan26WaktuSaatIni {
+
+    public static void main(String[] args) {
+
+       //  object tanggal
+       Date date = new Date();
+        
+       // menampilkan waktu dan tanggal menggunakan toString()
+       System.out.println("Hari ini adalah hari : " +date.toString());
+
+    }
+    
+}
